b7fd769cde4682a4bcfb0062c38e67670b8262ec
Trin Pongsri✌ <96470880+TinnyXP@users.noreply.github.com>
2025-02-23 15:22:01 +0700

    Create Blog with Sanity.io

--- schemaTypes/category.ts
+++ schemaTypes/category.ts
@@ -11,6 +11,16 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required()
+    }),
+    defineField({
       name: 'description',
       title: 'Description',
       type: 'text',
